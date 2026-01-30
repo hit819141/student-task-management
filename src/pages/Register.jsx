7eacde6fa86f35cc3a2d23c9db1c2c87c1e4d5a8
Bhalodiya Hit <bhalodiyahit21@gmail.com>
2026-01-30 13:48:14 +0530

    register file

---
+++ src/pages/Register.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+const Register = () => {
+  return (
+    <>
+      <div className="form-container">
+            <h1>Register Page</h1>
+        </div>
+    </>
+  );
+};
+
+export default Register;
