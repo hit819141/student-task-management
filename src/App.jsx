6c9cbaa3ba0a15983859b888ba6312825f16ce94
Bhalodiya Hit <bhalodiyahit21@gmail.com>
2026-02-02 13:35:39 +0530

    file updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
-import './App.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Login from "./pages/Login.jsx"
-import Register from "./pages/Register.jsx"
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/Header.jsx";
+import Register from "./pages/Register.jsx";
+import Login from "./pages/Login.jsx";
+import { createBrowserRouter, Navigate, Router, RouterProvider } from "react-router-dom";
 
+const DefaultRouter = () => {
+  const authData = JSON.parse(localStorage.getItem("authData"));
+  if(authData){
+  return<Navigate to = "/Login" replace/>;
+  }
+  return <Navigate to = "/Register" replace/>;
+}
 function App() {
   const route = createBrowserRouter([
     {
-      path:'/login',
-      element:<Login />
+      path: "/",
+      element: <DefaultRouter />
     },
     {
-      path: '/register',
-      element:<Register/>
-    }
-  ])
-  return <RouterProvider router={route}/>
-}
+      path: "/Login",
+      element: <Login />
+    },
+    {
+      path: "/Register",
+      element: <Register />
+    },
+  ]);
 
-export default App
+  return <RouterProvider router={route} />;
+}
+export default App;
