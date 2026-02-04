c64e89035eb746200efde81b43cb1e1ea6e9854a
Bhalodiya Hit <bhalodiyahit21@gmail.com>
2026-02-04 13:43:53 +0530

    all files are added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../index.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+        <div className="navbar-brand">
+        <h1>Task manager</h1>
+         </div>
+
+        <div className="navbar-actions">
+            <button className="btn.primary">
+            Add Task
+             </button>
+
+            <button className="btn-seccondary">
+            Logout
+            </button>
+         </div>
+    </nav>
+  )
+}
+
+export default Navbar
